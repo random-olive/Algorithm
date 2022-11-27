59253dc293793cb9f074396098c1c92be04d653e
random-olive <104327907+random-olive@users.noreply.github.com>
2022-11-27 19:31:52 +0900

    Programmers: for beginner: Lv.0 문자열 정렬하기 (2) complete

---
+++ Programmers/1_코딩테스트 입문 (Lv. 0)/8_문자열 정렬하기 (2).js
@@ -0,0 +1,24 @@
+function solution(my_string) {
+  return my_string.toLowerCase().split('').sort().join('');
+}
+
+
+//Try Again !
+function solution(my_string) {
+    return [...my_string.toLowerCase()].sort().join('')
+}
+
+
+/** TIL
+ * sort
+ * 
+ * spread?
+ * 
+ * 
+ ** EXAMPLE
+ * 
+ *
+ * 
+ *
+ *🥲 반성 : str -> arr : spread!
+ */
