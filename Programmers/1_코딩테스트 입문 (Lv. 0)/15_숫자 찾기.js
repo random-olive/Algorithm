08d83d62a8c2bc7c75abb57c3f2e9b251b3a3d1b
random-olive <104327907+random-olive@users.noreply.github.com>
2022-11-28 17:31:17 +0900

    Programmers: for beginner: Lv. 0 숫자 찾기 complete

---
+++ Programmers/1_코딩테스트 입문 (Lv. 0)/15_숫자 찾기.js
@@ -0,0 +1,36 @@
+function solution(num, k) {
+  let check = `${num}`.indexOf(k);
+  return check === -1 ? -1 : check + 1;
+}
+
+/** TIL
+ * Template literals : `${}`
+ * 
+ *
+ ** EXAMPLE
+ * Multi-line strings : 
+ * `string text line 1
+ *  string text line 2`
+ *  =>  "string text line 1
+ *       string text line 2"
+ * 
+ * Nesting templates : 
+ * const classes = `header ${ isLargeScreen() ? '' : `icon-${item.isCollapsed ? 'expander' : 'collapser'}` }`;
+ * 
+ * Tagged templates : 
+ * 
+ * var person = 'Mike'; var age = 28;
+ * 
+ * function myTag(strings, personExp, ageExp) {
+ *     var ageStr;
+ *     if (ageExp > 99){
+ *     ageStr = 'centenarian';
+ *     } else {
+ *     ageStr = 'youngster';
+ *     }
+ * 
+ *  return strings[0] + personExp + strings[1] + ageStr + strings[2]
+ * }
+ * 
+ * var output = myTag`that ${ person } is a ${ age }`;
+ */
