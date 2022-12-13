d95bd1d96615d08c35585ab00d5a14ddbfbf7acd
random-olive <104327907+random-olive@users.noreply.github.com>
2022-12-13 19:19:17 +0900

    Programmers: for beginner: Lv. 0 주사위의 개수 complete

---
+++ Programmers/1_코딩테스트 입문 (Lv. 0)/058_주사위의 개수.js
@@ -0,0 +1,13 @@
+function solution(box, n) {
+  return ~~(box[0] / n) * ~~(box[1] / n) * ~~(box[2] / n);
+}
+
+//Try Again !
+function solution(box, n) {
+  return box.reduce((a, c) => a * ~~(c / n), 1);
+}
+
+function solution(box, n) {
+  return box.map((el) => ~~(el / n)).reduce((a, c) => a * c, 1);
+}
+
