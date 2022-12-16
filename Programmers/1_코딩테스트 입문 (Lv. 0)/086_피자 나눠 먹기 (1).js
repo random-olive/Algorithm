8401ea00f3ac2ff67bc4b99a5bf8030ab151b11f
random-olive <104327907+random-olive@users.noreply.github.com>
2022-12-16 19:03:51 +0900

    Programmers: for beginner: Lv. 0 피자 나눠 먹기 (1) complete

---
+++ Programmers/1_코딩테스트 입문 (Lv. 0)/086_피자 나눠 먹기 (1).js
@@ -0,0 +1,17 @@
+function solution(n) {
+  return Math.ceil(n / 7);
+}
+
+//Try Again !
+function solution(n) {
+  return ((n / 7) << 0) + !!(n % 7);
+}
+
+function solution(n) {
+  return ~~(n / 7) + !!(n % 7);
+}
+
+/** TIL
+ * 🥲 반성 : #43
+ * 복습 : Operator : !!  => 확실한 논리결과를 가지기 위함 : 정의되지 않은 undefined 등의 값에도 확실한 true / false 값을 줌 + NOT
+ */
