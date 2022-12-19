387ca83aaa3688a388555824734c3206856f2c11
random-olive <104327907+random-olive@users.noreply.github.com>
2022-12-19 20:59:15 +0900

    Programmers: for beginner: Lv. 0 분수의 덧셈 complete

---
+++ Programmers/1_코딩테스트 입문 (Lv. 0)/092_분수의 덧셈.js
@@ -0,0 +1,27 @@
+function solution(denum1, num1, denum2, num2) {
+  let answer = [denum1 * num2 + denum2 * num1, num1 * num2];
+  let gcd = 1;
+  for (let i = 2; i <= num1 * num2; i++) {
+    answer[0] % i === 0 && answer[1] % i === 0 ? (gcd = i) : i;
+  }
+  return answer.map((el) => el / gcd);
+}
+
+//Try Again !
+function fnGCD(a, b) {
+  return a % b ? fnGCD(b, a % b) : b;
+}
+
+function solution(denum1, num1, denum2, num2) {
+  let denum = denum1 * num2 + denum2 * num1;
+  let num = num1 * num2;
+  let gcd = fnGCD(denum, num);
+
+  return [denum / gcd, num / gcd];
+}
+
+/** TIL
+ * 🥲 반성
+ * a % b === true (값이 있음) ?
+ * gcd * lcm = a * b
+ */
