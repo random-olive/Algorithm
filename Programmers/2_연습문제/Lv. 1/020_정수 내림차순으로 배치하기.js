980084f2d99804c353e5f6568cbdbafbee44e93d
random-olive <104327907+random-olive@users.noreply.github.com>
2022-12-27 15:43:12 +0900

    Programmers: practice : Lv. 1 정수 내림차순으로 배치하기 complete

---
+++ Programmers/2_연습문제/Lv. 1/020_정수 내림차순으로 배치하기.js
@@ -0,0 +1,23 @@
+function solution(n) {
+  return +[...`${n}`].sort((a, b) => b - a).join('');
+}
+
+//Try Again !
+function solution(n) {
+  return +(n + '').split('').sort().reverse().join('');
+}
+
+function solution(n) {
+  let nums = [];
+
+  while (n > 0) {
+    nums.push(n % 10);
+    n = ~~(n / 10);
+  }
+
+  return +nums.sort((a, b) => b - a).join('');
+}
+
+/** TIL
+ * sort(b-a) -> reverse(),  [`${}`] -> n+''.split('') => 속도 개선
+ */
