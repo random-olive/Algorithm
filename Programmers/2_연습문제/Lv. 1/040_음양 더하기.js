71d87d63f201bc388fe38bf7b9396572a8d86d2e
random-olive <104327907+random-olive@users.noreply.github.com>
2023-01-04 14:41:11 +0900

    Programmers: practice : Lv. 1 음양 더하기 complete

---
+++ Programmers/2_연습문제/Lv. 1/040_음양 더하기.js
@@ -0,0 +1,19 @@
+function solution(absolutes, signs) {
+  let result = 0;
+  signs.forEach((el, i) => (result += absolutes[i] * (el ? 1 : -1)));
+  return result;
+}
+
+//Fixed !
+function solution(absolutes, signs) {
+  return signs.reduce((a, c, i) => (a += c ? absolutes[i] : -absolutes[i]), 0);
+}
+
+//Another !
+function solution(absolutes, signs) {
+  return signs.reduce((a, c, i) => (c ? (a += absolutes[i]) : (a -= absolutes[i])), 0);
+}
+
+function solution(absolutes, signs) {
+  return absolutes.reduce((a, c, i) => a + c * (signs[i] ? 1 : -1), 0);
+}
