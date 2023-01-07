b9ad51201b9e2eb4731e7377ed684e52fe8c0fb2
random-olive <104327907+random-olive@users.noreply.github.com>
2023-01-07 22:44:26 +0900

    Programmers: practice : Lv. 1 예산 complete

--- Programmers/2_연습문제/Lv. 1/035_예산.js
+++ Programmers/2_연습문제/Lv. 1/035_예산.js
@@ -1,11 +1,61 @@
 function solution(d, budget) {
   let result = 0;
-  
   d = d.sort((a, b) => a - b);
 
-  for (let i = 0; i < d.length; i++) {
+  for (var i = 0; i < d.length; i++) {
     result += d[i];
     if (result > budget) return i;
-    else if (result === budget) return i + 1;
   }
+  return i;
 }
+
+//Try Again !
+function solution(d, budget) {
+  d = d.sort((a, b) => a - b);
+  for (var i = 0; i < d.length; i++) {
+    if (budget < d[i]) break;
+    budget -= d[i];
+  }
+  return i;
+}
+
+//??
+function solution(d, budget) {
+  return ~(~d.sort((a, b) => a - b).map((v) => (budget -= v)).findIndex((v) => v < 0) || ~d.length);
+}
+
+//
+function solution(d, budget) {
+  d.sort((a, b) => a - b);
+  while (d.reduce((a, c) => a + c, 0) > budget) d.pop();
+  return d.length;
+}
+
+//
+function solution(d, budget) {
+  let answer = 0;
+  let money = 0;
+  d.sort((a, b) => a - b).forEach(function (el) {
+    money += el;
+    if (money <= budget) {
+      answer++;
+    }
+  });
+  return answer;
+}
+
+//**
+function solution(d, budget) {
+  return d.sort((a, b) => a - b).reduce((count, price) => {return count + ((budget -= price) >= 0)}, 0);
+}
+
+//input itself : none -> true : false
+var solution = (d, budget) =>
+  d.sort((a, b) => a - b).filter((el) => (budget >= el ? ((budget = budget - el), 1) : 0)).length;
+
+/** TIL
+ * ~ : Bitwise NOT : bit를 NOT처리 (반대) : it converts the operand to a 32-bit signed integer
+ *
+ * 🥲 반성
+ * var 활용
+ */
