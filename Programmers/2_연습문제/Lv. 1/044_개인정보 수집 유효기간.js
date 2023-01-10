e13f9fa4c2a9c3275771927bc693d461b0c45d0c
random-olive <104327907+random-olive@users.noreply.github.com>
2023-01-10 23:13:52 +0900

    Programmers: practice : Lv. 1 개인정보 수입 유효기간 -동일 month check  logic

--- Programmers/2_연습문제/Lv. 1/044_개인정보 수집 유효기간.js
+++ Programmers/2_연습문제/Lv. 1/044_개인정보 수집 유효기간.js
@@ -1,7 +1,9 @@
 function solution(today, terms, privacies) {
   terms = terms.map((el) => el.split(' '));
   privacies = privacies.map((el) => el.split(' '));
-return privacies.map(p=>terms.map(t=>p[1]===t[0]? p[1]=p[1].replace(t[0],t[1]):''))
+  return privacies.map((p) =>
+    terms.map((t) => (p[1] === t[0] ? (p[1] = p[1].replace(t[0], t[1])) : ''))
+  );
 }
 
 // "2022.05.19"
@@ -13,7 +15,53 @@ return privacies.map(p=>terms.map(t=>p[1]===t[0]? p[1]=p[1].replace(t[0],t[1]):'
 
 function solution(today, terms, privacies) {
   terms = terms.map((el) => el.split(' '));
-  return privacies = privacies
+  return (privacies = privacies
     .map((p) => (p = p.replace(' ', '.')))
-    .map((p) => terms.map((t) => (p.includes(t[0]) ? p.replace(t[0], t[1]) : ''))).map(el=>el.filter(v=>v)).map(el=>el)
+    .map((p) =>
+      terms.map((t) => (p.includes(t[0]) ? p.replace(t[0], t[1]) : ''))
+    )
+    .map((el) => el.filter((v) => v)));
+}
+
+function solution(today, terms, privacies) {
+  terms = terms.map((el) => el.split(' '));
+  terms = Object.entries(obj).forEach(([key, value]) => {
+    key: value;
+  });
+  // return (privacies = privacies.map((p) => p.replace(' ', '.').split('.')));
+  return terms;
+}
+
+function solution(today, terms, privacies) {
+  let obj = {};
+  terms = terms.map((el) => el.split(' ')).map((t) => (obj[t[0]] = t[1])); //{'A':'6','B':'7'}
+  privacies = privacies.map((el) => el.replace(' ', '.'));
+  for (let i = 0; i < privacies.length; i++) {
+    for (let j = 0; j < terms.length; j++) {}
+  }
+
+  // .map((p) => p.replace('D', '5')).map((p) => p.replace('Z', '3'))
+  // .split('.')); //["2019","01","01","D"]
+  // return privacies.map((p) => (p[3] = obj[p[3]]));
+}
+
+//
+function solution(today, terms, privacies) {
+  (terms = terms.map((el) => el.split(' '))).sort();
+  let list = terms.map((t) => t[0]).join(''); //['D','Z']
+  terms = terms.map((t) => +t[1]); //[5,3]
+  privacies = privacies
+    .map((p) => p.replace(' ', '.'))
+    .map((p) =>
+      p.replace(
+        list[list.indexOf(p[p.length - 1])],
+        terms[list.indexOf(p[p.length - 1])]
+      )
+    )
+    .map(v=>v.split('.')).map(el=>`${Number(el[0])+Math.floor((Number(el[1])+Number(el[3]))/12? (Number(el[1])+Number(el[3]))/12:0)}.${(Number(el[1])+Number(el[3]))%12? (Number(el[1])+Number(el[3]))%12:12}.${el[2]}`) 
+   
+    //2019,2019,2019,2019,2019,2018
+    //6,14,13,12,15
+    return privacies
+    // return privacies.filter((p,i)=>new Date(p)-new Date(today)<0)
 }
